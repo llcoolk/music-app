e93c49e2c2282aca44ca6d0cd9a72027a9470794
Leen Kim <llcoolk@MACBKPROi7.fios-router.home>
2018-08-31 01:18:40 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./App.css";
+import { FormGroup, FormControl, InputGroup } from "react-bootstrap";
+import Profile from "./Profile";
+import Gallery from "./Gallery";
+
+class App extends Component {
+  state = {
+    query: "",
+    artist: null,
+    tracks: []
+  };
+
+  search() {
+    console.log("this.state", this.state);
+    const BASE_URL = "https://api.spotify.com/v1/search?";
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    // console.log("FETCH_URL", FETCH_URL);
+    const ALBUM_URL = "http://api.spotify.com/v1/artists/";
+
+    fetch(FETCH_URL, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(json => {
+        const artist = json.artists.items[0];
+        console.log("artist", artist);
+        this.setState({ artist });
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`;
+        fetch(FETCH_URL, {
+          method: "GET"
+        })
+          .then(response => response.json())
+          .then(json => {
+            console.log("artist's top tracks: ", json);
+            const { tracks } = json;
+            // const tracks = tracks.json;
+            this.setState({ tracks });
+          });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music App</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => this.setState({ query: event.target.value })}
+              onKeyPress={event => {
+                if (event.key === "Enter") {
+                  this.search();
+                }
+              }}
+            />
+            <button onClick={() => this.search()}>Search</button>
+          </InputGroup>
+        </FormGroup>
+        {this.state.artist !== null ? (
+          <div>
+            <Profile artist={this.state.artist} />
+            <Gallery tracks={this.state.tracks} />
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
